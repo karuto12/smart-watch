bae9d9253166bd1d53af16fd9d58ed97b32e2990
karuto12 <haridasmahato12@gmail.com>
2025-01-05 17:11:29 +0530

    renamed few files and folders and made file to train the model

---
+++ smart-watch/src/main.py
@@ -0,0 +1,4 @@
+from src.modules.detection import misbehavior_detection
+from src.modules.config import setup_notification_methods
+from src.DVR_handlers import hybrid_handler
+from src.modules.notifications import notification_manager
